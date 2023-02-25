0ad9413f6e0cdc487ee2466fea773de2f4289f88
Aleksei Ivanov <contact@aleksei.dev>
2023-02-25 23:17:49 +0300

    Update data.go

--- internal/ws/data.go
+++ internal/ws/data.go
@@ -1,7 +1,27 @@
 package ws
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 type RobocatDataFields struct {
-	Path     string `json:"path"`
+	Path string `json:"path"`
+	// Mime-Type of the file (set only for outputs to aid decoding the payload).
 	MimeType string `json:"type"`
 	Payload  []byte `json:"payload"`
+}
+
+func ParseDataFields(m *Message) (*RobocatDataFields, error) {
+	if m.Name != "input" {
+		return nil, errors.New("message is not of type 'input'")
+	}
+
+	var fields *RobocatDataFields
+	err := json.Unmarshal(m.Body, &fields)
+	if err != nil {
+		return nil, err
+	}
+
+	return fields, nil
 }
