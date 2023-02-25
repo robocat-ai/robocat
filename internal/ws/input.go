7d4d2134222aba19bbdff8d6e5433e76f9d6d2f7
Aleksei Ivanov <contact@aleksei.dev>
2023-02-25 23:17:53 +0300

    Update input.go

--- internal/ws/input.go
+++ internal/ws/input.go
@@ -1,14 +1,18 @@
 package ws
 
-import "context"
+import (
+	"context"
+	"os"
+	"path"
+)
 
 type RobocatInput struct {
-	//
+	runner *RobocatRunner
 }
 
-func NewRobocatInput() *RobocatInput {
+func NewRobocatInput(runner *RobocatRunner) *RobocatInput {
 	return &RobocatInput{
-		//
+		runner: runner,
 	}
 }
 
@@ -16,5 +20,33 @@ func (r *RobocatInput) Handle(
 	ctx context.Context,
 	message *Message,
 ) {
-	//
+	fields, err := ParseDataFields(message)
+	if err != nil {
+		message.ReplyWithError(err)
+		return
+	}
+
+	inputBasePath, err := r.runner.GetFlowBasePath("input")
+	if err != nil {
+		message.ReplyWithError(err)
+		return
+	}
+
+	filePath := path.Join(inputBasePath, fields.Path)
+
+	err = os.MkdirAll(path.Dir(filePath), 0755)
+	if err != nil {
+		message.ReplyWithError(err)
+		return
+	}
+
+	err = os.WriteFile(filePath, fields.Payload, 0644)
+	if err != nil {
+		message.ReplyWithError(err)
+		return
+	}
+
+	message.Reply("status", "ok")
+
+	log.Debugw("Written input", "path", fields.Path)
 }
