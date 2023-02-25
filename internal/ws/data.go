d234541c76a296e16ecf871a75960a4d3e2ddc4e
Aleksei Ivanov <contact@aleksei.dev>
2023-02-25 22:25:49 +0300

    Update data.go

--- internal/ws/data.go
+++ internal/ws/data.go
@@ -2,6 +2,6 @@ package ws
 
 type RobocatDataFields struct {
 	Path     string `json:"path"`
-	MimeType string `json:"mime-type"`
+	MimeType string `json:"type"`
 	Payload  []byte `json:"payload"`
 }
