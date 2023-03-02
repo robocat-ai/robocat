dcc3dc68a6a0f2f8219f756720c21f7bebb6bc34
Aleksei Ivanov <contact@aleksei.dev>
2023-03-02 22:54:55 +0300

    Implement output stream

--- internal/ws/file.go
+++ internal/ws/file.go
@@ -1,7 +1,13 @@
 package ws
 
 import (
+	"bytes"
 	"encoding/json"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"mime"
+	"strings"
 )
 
 type RobocatFile struct {
@@ -19,4 +25,32 @@ func ParseFileFromMessage(m *Message) (*RobocatFile, error) {
 	}
 
 	return fields, nil
+}
+
+// Complete MIME-type as provided by mime.ParseMediaType.
+func (f *RobocatFile) Type() string {
+	typ, _, err := mime.ParseMediaType(f.MimeType)
+	if err != nil {
+		return ""
+	}
+
+	return typ
+}
+
+// Generic file kind extracted from MIME-type (i.e. text, image, etc).
+func (f *RobocatFile) Kind() string {
+	typ := f.Type()
+
+	return strings.Trim(strings.Split(typ, "/")[0], "/")
+}
+
+// Converts transmitted payload to plain text and returns it as string.
+func (f *RobocatFile) Text() string {
+	return string(f.Payload)
+}
+
+// Uses image.Decode to decode an image that has been encoded in a registered
+// format. The string returned is the format name.
+func (f *RobocatFile) Image() (image.Image, string, error) {
+	return image.Decode(bytes.NewReader(f.Payload))
 }
