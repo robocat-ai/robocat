4c30a3c3e278d153427e468331c1f8723a70667e
Aleksei Ivanov <contact@aleksei.dev>
2023-03-03 13:36:01 +0300

    Update command_flow.go and flow.go

--- pkg/client/command_flow.go
+++ pkg/client/command_flow.go
@@ -1,6 +1,8 @@
 package robocat
 
 import (
+	"context"
+	"errors"
 	"time"
 
 	"github.com/robocat-ai/robocat/internal/ws"
@@ -35,4 +37,64 @@ func (chain *FlowCommandChain) WithProxy(proxy string) *FlowCommandChain {
 func (chain *FlowCommandChain) WithTimeout(timeout time.Duration) *FlowCommandChain {
 	chain.timeout = timeout
 	return chain
+}
+
+func (chain *FlowCommandChain) Run() *RobocatFlow {
+	flow := &RobocatFlow{
+		client: chain.client,
+		log:    &RobocatLogStream{},
+		output: &RobocatFileStream{},
+	}
+
+	ref, err := chain.client.sendCommand("run", chain.args)
+	if err != nil {
+		flow.err = err
+		return flow
+	}
+
+	ctx, cancel := context.WithTimeout(chain.client.ctx, chain.timeout)
+
+	flow.ref = ref
+	flow.ctx = ctx
+
+	chain.client.subscribe(flow.ref, func(ctx context.Context, m *ws.Message) {
+		if m.Name == "status" {
+			if m.MustText() == "success" {
+				cancel()
+			}
+		} else if m.Name == "log" {
+			flow.log.Push(m.MustText())
+		} else if m.Name == "error" {
+			flow.err = errors.New(m.MustText())
+			cancel()
+		} else if m.Name == "output" {
+			file, err := ws.ParseFileFromMessage(m)
+			if err != nil {
+				flow.err = err
+				cancel()
+			}
+
+			flow.output.Push(file)
+		}
+	})
+
+	go func() {
+		defer flow.Close()
+		defer cancel()
+
+		for {
+			select {
+			case <-ctx.Done():
+				if ctx.Err() == context.DeadlineExceeded {
+					flow.err = context.DeadlineExceeded
+				}
+				return
+			case <-chain.client.CancelFlow():
+				flow.err = errors.New("flow was aborted")
+				return
+			}
+		}
+	}()
+
+	return flow
 }
