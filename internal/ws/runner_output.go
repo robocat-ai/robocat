43f431d2714b6265587cc83c1656026f45b1dfaf
Aleksei Ivanov <contact@aleksei.dev>
2023-03-08 15:45:45 +0300

    Fix output watcher
    Gracefully restart output path watcher when watched directory is removed

--- internal/ws/runner_output.go
+++ internal/ws/runner_output.go
@@ -11,11 +11,12 @@ import (
 	"github.com/radovskyb/watcher"
 )
 
-func (r *RobocatRunner) watchOutput(
+func (r *RobocatRunner) watchOutputPath(
 	ctx context.Context,
 	message *Message,
-) {
-	outputBasePath, err := r.GetFlowBasePath("output")
+	path string,
+) error {
+	outputBasePath, err := r.GetFlowBasePath(path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,58 +24,12 @@ func (r *RobocatRunner) watchOutput(
 	err = os.MkdirAll(outputBasePath, 0755)
 	if err != nil {
 		log.Warn(err)
-		return
+		return err
 	}
 
 	w := watcher.New()
 
 	w.FilterOps(watcher.Create, watcher.Write)
-
-	go func() {
-		for {
-			select {
-			case event := <-w.Event:
-				if event.IsDir() {
-					continue
-				}
-
-				log.Debugw("Got output update", "path", event.Path)
-
-				path, err := filepath.Rel(outputBasePath, event.Path)
-				if err != nil {
-					log.Warnw("Unable to form relative path", "error", err)
-					continue
-				}
-
-				ext := filepath.Ext(path)
-				if len(ext) == 0 {
-					ext = ".txt"
-				}
-
-				mimeType := mime.TypeByExtension(ext)
-
-				payload, err := os.ReadFile(event.Path)
-				if err != nil {
-					log.Warnw("Unable to read file", "error", err, "file", event.Path)
-					continue
-				}
-
-				if ext == ".txt" {
-					payload = bytes.TrimSpace(payload)
-				}
-
-				message.Reply("output", RobocatFile{
-					Path:     path,
-					MimeType: mimeType,
-					Payload:  payload,
-				})
-			case err := <-w.Error:
-				log.Fatalln(err)
-			case <-w.Closed:
-				return
-			}
-		}
-	}()
 
 	if err := w.AddRecursive(outputBasePath); err != nil {
 		log.Fatal(err)
@@ -87,10 +42,75 @@ func (r *RobocatRunner) watchOutput(
 	}()
 	defer w.Close()
 
-	log.Debug("Watching output")
+	log.Debugf("Watching directory recusively: %s", outputBasePath)
 
-	// Wait until context is cancelled.
-	<-ctx.Done()
+	for {
+		select {
+		case event := <-w.Event:
+			if event.IsDir() {
+				continue
+			}
+
+			log.Debugw("Got output update", "path", event.Path)
+
+			path, err := filepath.Rel(outputBasePath, event.Path)
+			if err != nil {
+				log.Warnw("Unable to form relative path", "error", err)
+				continue
+			}
+
+			ext := filepath.Ext(path)
+			if len(ext) == 0 {
+				ext = ".txt"
+			}
+
+			mimeType := mime.TypeByExtension(ext)
+
+			payload, err := os.ReadFile(event.Path)
+			if err != nil {
+				log.Warnw("Unable to read file", "error", err, "file", event.Path)
+				continue
+			}
+
+			if ext == ".txt" {
+				payload = bytes.TrimSpace(payload)
+			}
+
+			message.Reply("output", RobocatFile{
+				Path:     path,
+				MimeType: mimeType,
+				Payload:  payload,
+			})
+		case err := <-w.Error:
+			if err == watcher.ErrWatchedFileDeleted {
+				log.Debugf("Output directory was removed: %s", outputBasePath)
+				return nil
+			} else {
+				return err
+			}
+		case <-ctx.Done():
+		case <-w.Closed:
+			return nil
+		}
+	}
+}
+
+func (r *RobocatRunner) watchOutput(
+	ctx context.Context,
+	message *Message,
+) {
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			break loop
+		default:
+			err := r.watchOutputPath(ctx, message, "output")
+			if err != nil {
+				log.Warnf("Got output watcher error: %w", err)
+			}
+		}
+	}
 
 	log.Debug("Stopped watching output")
 }
